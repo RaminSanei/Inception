6f93d822fa908320e577da30a20d480d67146e88
Ramin  Sanei <raminsanei1992@gmail.com>
2025-05-06 19:18:54 +0000

    Add director.

--- srcs/requirements/mariadb/tools/start.sh
+++ srcs/requirements/mariadb/tools/start.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-if [ ! -d "/var/lib/mysql/mysql" ]; then
-  mysql_install_db --user=$DB_USER --datadir=/var/lib/mysql
+MYSQL_DATA_DIR="/var/lib/mysql"
+MYSQL_DB_DIR="$MYSQL_DATA_DIR/mysql"
+
+if [ ! -d '${MYSQL_DB_DIR}' ]; then
+  mysql_install_db --user=$DB_USER --datadir='${MYSQL_DATA_DIR}'
 fi
 
 cat << EOF > /etc/mysql/init.sql
 CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;
 CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';
 GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO '${DB_USER}'@'%' WITH GRANT OPTION;
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}';
 FLUSH PRIVILEGES;
 EOF
 
