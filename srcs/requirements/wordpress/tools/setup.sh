d353979bf09d3a67cd99ae3db72e404ee5887f01
Ramin  Sanei <raminsanei1992@gmail.com>
2025-05-06 23:15:17 +0000

    improve setup5.

--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -1,9 +1,8 @@
-# #!/bin/bash
+#!/bin/bash
 
-WP_URL="https://ssanei.42.fr"
-WEB_ROOT=/var/www/html
+wp_url = https://ssanei.42.fr;
 
-cd $WEB_ROOT
+cd /var/www/html
 
 if [ ! -f /var/www/html/wp-config.php ]; then
     wp core download --allow-root
@@ -12,47 +11,25 @@ fi
 
 sleep 10
 
-# wp config create --url="$DOMAIN_NAME" --dbname="$DB_NAME" --dbuser="$DB_USER" --dbpass="$DB_PASSWORD" --dbhost="mariadb" --allow-root
+wp config create --url="$DOMAIN_NAME" --dbname="$DB_NAME" --dbuser="$DB_USER" --dbpass="$DB_PASSWORD" --dbhost="mariadb" --allow-root
 
-# wp core install --url=$DOMAIN_NAME --title=$WP_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --allow-root
+wp core install --url=$DOMAIN_NAME --title=$WP_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --allow-root
 
-# wp user create $WP_USER $WP_USER_EMAIL --user_pass=$WP_USER_PASSWORD --role=author --allow-root
-wp config create \
-    --url="$DOMAIN_NAME" \
-    --dbname="$DB_NAME" \
-    --dbuser="$DB_USER" \
-    --dbpass="$DB_PASSWORD" \
-    --dbhost="mariadb" \
-    --allow-root
+wp user create $WP_USER $WP_USER_EMAIL --user_pass=$WP_USER_PASSWORD --role=author --allow-root
 
-# Install WordPress core
-wp core install \
-    --url="$DOMAIN_NAME" \
-    --title="$WP_TITLE" \
-    --admin_user="$WP_ADMIN_USER" \
-    --admin_password="$WP_ADMIN_PASSWORD" \
-    --admin_email="$WP_ADMIN_EMAIL" \
-    --allow-root
 
-# Create additional author user
-wp user create "$WP_USER" "$WP_USER_EMAIL" \
-    --user_pass="$WP_USER_PASSWORD" \
-    --role=author \
-    --allow-root
+wp option update home "$wp_url" --allow-root --quiet
+wp option update siteurl "$wp_url" --allow-root --quiet
 
-wp option update home "$WP_URL" --allow-root --quiet
-wp option update siteurl "$WP_URL" --allow-root --quiet
-UPLOADS_DIR=wp-content/uploads
-
-find $UPLOADS_DIR -type d -exec chmod 755 {} \;   # Directories: 755
-find $UPLOADS_DIR -type f -exec chmod 644 {} \;   # Files: 644
-chown -R www-data:www-data $UPLOADS_DIR
+find wp-content/uploads -type d -exec chmod 755 {} \;   # Directories: 755
+find wp-content/uploads -type f -exec chmod 644 {} \;   # Files: 644
+chown -R www-data:www-data wp-content/uploads
 
 
 php-fpm7.3 -F
 
 
-#//************************************************************
+//************************************************************
 # WP_URL="https://ssanei.42.fr"
 # WEB_ROOT=/var/www/html
 
@@ -103,4 +80,4 @@ php-fpm7.3 -F
 # # fi
 
 # # Start PHP-FPM
-exec php-fpm7.3 -F
+# exec php-fpm7.3 -F
