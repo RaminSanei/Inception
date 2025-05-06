c947e2dbd7dcf82ccb226640fb68ca5e7a987743
Ramin  Sanei <raminsanei1992@gmail.com>
2025-05-06 21:30:42 +0000

    wp docker change2.

--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -7,16 +7,16 @@ if [ ! -f /var/www/html/wp-config.php ]; then
 fi
 
 
-cp wp-config-sample.php wp-config.php
+# cp wp-config-sample.php wp-config.php
 
-sed -i -r "s/database_name_here/$DB_NAME/" wp-config.php
-sed -i -r "s/username_here/$DB_USER/" wp-config.php
-sed -i -r "s/password_here/$DB_PASSWORD/" wp-config.php
-sed -i -r "s/localhost/mariadb/" wp-config.php
+# sed -i -r "s/database_name_here/$DB_NAME/" wp-config.php
+# sed -i -r "s/username_here/$DB_USER/" wp-config.php
+# sed -i -r "s/password_here/$DB_PASSWORD/" wp-config.php
+# sed -i -r "s/localhost/mariadb/" wp-config.php
 
 
 sleep 10
-# wp config create --url="$DOMAIN_NAME" --dbname="$DB_NAME" --dbuser="$DB_USER" --dbpass="$DB_PASSWORD" --dbhost="mariadb" --allow-root
+wp config create --url="$DOMAIN_NAME" --dbname="$DB_NAME" --dbuser="$DB_USER" --dbpass="$DB_PASSWORD" --dbhost="mariadb" --allow-root
 
 wp core install --url=$DOMAIN_NAME --title=$WP_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --allow-root
 
