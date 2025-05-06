2cffc1c3bbaa2db22b482aa0d5ce4933d2a9f850
Ramin  Sanei <raminsanei1992@gmail.com>
2025-05-06 23:55:52 +0000

    improve setup7.

--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -11,7 +11,6 @@ fi
 
 sleep 10
 
-# wp config create --url="$DOMAIN_NAME" --dbname="$DB_NAME" --dbuser="$DB_USER" --dbpass="$DB_PASSWORD" --dbhost="mariadb" --allow-root
 wp config create \
     --url="$DOMAIN_NAME" \
     --dbname="$DB_NAME" \
@@ -19,7 +18,7 @@ wp config create \
     --dbpass="$DB_PASSWORD" \
     --dbhost="mariadb" \
     --allow-root
-# wp core install --url=$DOMAIN_NAME --title=$WP_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --allow-root
+
 wp core install \
     --url="$DOMAIN_NAME" \
     --title="$WP_TITLE" \
@@ -27,7 +26,7 @@ wp core install \
     --admin_password="$WP_ADMIN_PASSWORD" \
     --admin_email="$WP_ADMIN_EMAIL" \
     --allow-root
-# wp user create $WP_USER $WP_USER_EMAIL --user_pass=$WP_USER_PASSWORD --role=author --allow-root
+
 wp user create "$WP_USER" "$WP_USER_EMAIL" \
     --user_pass="$WP_USER_PASSWORD" \
     --role=author \
@@ -43,57 +42,3 @@ chown -R www-data:www-data wp-content/uploads
 
 
 php-fpm7.3 -F
-
-
-//************************************************************
-# WP_URL="https://ssanei.42.fr"
-# WEB_ROOT=/var/www/html
-
-# cd $WEB_ROOT
-
-# # Download WordPress core only if not already present
-# if [ ! -f wp-config.php ]; then
-#     wp core download --allow-root
-# fi
-
-# # Wait for DB container to be ready (can be replaced with a proper health check)
-# sleep 10
-
-# # Create wp-config.php with environment variables
-# wp config create \
-#     --url="$DOMAIN_NAME" \
-#     --dbname="$DB_NAME" \
-#     --dbuser="$DB_USER" \
-#     --dbpass="$DB_PASSWORD" \
-#     --dbhost="mariadb" \
-#     --allow-root
-
-# # Install WordPress core
-# wp core install \
-#     --url="$DOMAIN_NAME" \
-#     --title="$WP_TITLE" \
-#     --admin_user="$WP_ADMIN_USER" \
-#     --admin_password="$WP_ADMIN_PASSWORD" \
-#     --admin_email="$WP_ADMIN_EMAIL" \
-#     --allow-root
-
-# # Create additional author user
-# wp user create "$WP_USER" "$WP_USER_EMAIL" \
-#     --user_pass="$WP_USER_PASSWORD" \
-#     --role=author \
-#     --allow-root
-
-# # Update home and site URL
-# wp option update home "$WP_URL" --allow-root --quiet
-# wp option update siteurl "$WP_URL" --allow-root --quiet
-
-# # Set secure file/directory permissions for uploads
-# UPLOADS_DIR=wp-content/uploads
-# # if [ -d "$UPLOADS_DIR" ]; then
-#     find "$UPLOADS_DIR" -type d -exec chmod 755 {} \;
-#     find "$UPLOADS_DIR" -type f -exec chmod 644 {} \;
-#     chown -R www-data:www-data "$UPLOADS_DIR"
-# # fi
-
-# # Start PHP-FPM
-# exec php-fpm7.3 -F
